889fe27ec79a56ab9fdc5c75952ac75e286ad71a
17AngelAlbertoMayCatzin <a.a.m.catzin@gmail.com>
2021-07-14 00:58:59 -0500

    Aplicaciones como evidencia de la materia de Programación de dispositivos moviles

---
+++ hello-world/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 >Mi primera aplicación</h1>
+        <h2>Probando funcionamiento</h2>
+      </header>
+      <main>
+        <p>
+          Se usted bienvenido
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
